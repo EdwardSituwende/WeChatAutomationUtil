ee1ee22fa1aa957671467824f01d937ede1a3480
Edward <lizh@healthmall.cn>
2018-01-27 17:14:29 +0800

    update

--- app/src/main/java/per/edward/wechatautomationutil/MainActivity.java
+++ app/src/main/java/per/edward/wechatautomationutil/MainActivity.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edit,editIndex, editCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,17 +18,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        findViewById(R.id.open_accessibility_setting).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                switch (v.getId()) {
-                    case R.id.open_accessibility_setting:
-                        OpenAccessibilitySettingHelper.jumpToSettingPage(getBaseContext());
-                        break;
-                }
-            }
-        });
+        edit = findViewById(R.id.edit);
+        editIndex = findViewById(R.id.edit_index);
+        editCount = findViewById(R.id.edit_count);
+
+        findViewById(R.id.open_accessibility_setting).setOnClickListener(clickListener);
+        findViewById(R.id.btn_save).setOnClickListener(clickListener);
     }
 
+    View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.open_accessibility_setting:
+                    OpenAccessibilitySettingHelper.jumpToSettingPage(getBaseContext());
+                    break;
+                case R.id.btn_save:
+                    break;
+            }
+        }
+    };
 
 }
